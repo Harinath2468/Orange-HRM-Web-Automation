d4f650050f04f5873d636ea66f88128553531293
Harinath2468 <106509769+Harinath2468@users.noreply.github.com>
2025-06-05 00:09:00 +0530

    Update AdminPage.java

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import Abstractcomponents.Utils;
 import customExceptions.EmployeeNotFoundException;
 
