1f5fb3ec3c62d48f31ca10ab2ce9d3a477bb0cb0
Harinath2468 <106509769+Harinath2468@users.noreply.github.com>
2025-06-05 00:09:15 +0530

    Update LandingDashboardPage.java

--- src/main/java/pages/LandingDashboardPage.java
+++ src/main/java/pages/LandingDashboardPage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import Abstractcomponents.Utils;
 
 
